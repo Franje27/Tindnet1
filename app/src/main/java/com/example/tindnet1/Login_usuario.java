f64afacc9c8a569e7c3b1d87464e4c31d28bfcd8
Franje27 <franje27@gmail.com>
2024-05-03 14:40:13 +0200

    Commit con cambios: Enlazado de registro de usuario con su página correspondiente

---
+++ app/src/main/java/com/example/tindnet1/Login_usuario.java
@@ -0,0 +1,15 @@
+package com.example.tindnet1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login_usuario extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_usuario);
+
+
+    }
+}
