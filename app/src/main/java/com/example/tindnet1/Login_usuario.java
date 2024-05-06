44c2d542a10ee40ba982e246aa9f9025e063a684
Franje27 <franje27@gmail.com>
2024-05-06 14:56:59 +0200

    Commit: Vincular con firebase y guardar los usuarios que se registran

--- app/src/main/java/com/example/tindnet1/Login_usuario.java
+++ app/src/main/java/com/example/tindnet1/Login_usuario.java
@@ -1,15 +1,90 @@
 package com.example.tindnet1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login_usuario extends AppCompatActivity {
+    private static final String TAG="Login_usuario";
+    private EditText editTextName, editTextEmail, editTextPassword;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_usuario);
 
 
+// ...
+// Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // Obtención de referencias a los EditTexts
+        editTextName = findViewById(R.id.editTextText2);
+        editTextEmail = findViewById(R.id.editTextTextEmailAddress2);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+
+        Button buttonCreateAccount = findViewById(R.id.button5);
+        buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// Obtención de los datos de los EditTexts
+                String name = editTextName.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+// Llamada al método createUserWithEmailAndPassword con los datos obtenidos
+                createUserWithEmailAndPassword(email, password);
+            }
+        });
+    }
+    private void createUserWithEmailAndPassword(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+// Creación de usuario exitosa
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+// Si la creación de usuario falla, muestra un mensaje al usuario
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Login_usuario.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+    private void updateUI(FirebaseUser user) {
+// Aquí puedes actualizar la interfaz de usuario según el estado de autenticación
+// Por ejemplo, puedes redirigir al usuario a otra actividad después de que haya iniciado sesión correctamente
+        if (user != null) {
+            Intent intent = new Intent(Login_usuario.this, MainActivity.class); //Cambiarlo por la página siguiente
+            startActivity(intent);
+            finish();
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
     }
 }
