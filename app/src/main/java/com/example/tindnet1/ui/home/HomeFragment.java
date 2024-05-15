2c52bc6a9a23849526cdc17ee82c14f38310a485
Franje27 <franje27@gmail.com>
2024-05-15 15:05:33 +0200

    Commit: Diseño tarjetas slide, no he conseguido que se vea solo en home, por ahora se ve en home, dashboard y settings.

--- app/src/main/java/com/example/tindnet1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/tindnet1/ui/home/HomeFragment.java
@@ -10,14 +10,28 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.tindnet1.Company;
+import com.example.tindnet1.CompanyAdapter;
+import com.example.tindnet1.R;
 import com.example.tindnet1.databinding.FragmentHomeBinding;
+// Clases importadas para lo de las tarjetas
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+      private ViewPager2 viewPager; // para lo de las tarjetas
+       private CompanyAdapter adapter;  //para lo de las tarjetas
+
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+                              ViewGroup container,  Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
@@ -26,8 +40,30 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //Cambio para probar tarjetas  return root;
+
+       /*
+       // codigo para lo de las tarjetas
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        viewPager = view.findViewById(R.id.viewPager);
+
+
+        List<Company> companies = new ArrayList<>();
+        companies.add(new Company(R.drawable.foto_empresa_random, "Description for Company 1"));
+        companies.add(new Company(R.drawable.random_foto_2, "Description for Company 2"));
+        // Añade más empresas según sea necesario
+
+        adapter = new CompanyAdapter(companies);
+        viewPager.setAdapter(adapter);
+
+        return view;
+*/
         return root;
     }
+
+
+
 
     @Override
     public void onDestroyView() {
